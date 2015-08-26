22ad617f38c964fc05947d5bb936456ac47ae68d
Zack Slayton <zslayton@us.ibm.com>
2015-08-26 12:02:06 -0400

    Base types.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
+use std::result::Result;
+
+pub struct BytesConsumed(usize);
+pub struct BytesProduced(usize);
+
+pub enum DecodingError {
+  InvalidFrame(BytesConsumed),
+  IncompleteFrame
+}
+
+pub enum EncodingError {
+  InvalidFrame,
+  InsufficientBufferSize
+}
+
+pub struct DecodedFrame<F> {
+  frame: F,
+  bytes_consumed: BytesConsumed
+}
+
+pub struct EncodedFrame<F> {
+  frame: F,
+  bytes_produced: BytesProduced
+}
+
+pub type DecodingResult<F> = Result<Option<DecodedFrame<F>>, DecodingError>;
+pub type EncodingResult<F> = Result<Option<EncodedFrame<F>>, EncodingError>;
+
+pub trait Codec<F> {
+  fn encode(&mut self, &mut [u8]) -> EncodingResult<F>;
+  fn decode(&mut self, &[u8]) -> DecodingResult<F>;
+}
+
 #[test]
 fn it_works() {
 }
