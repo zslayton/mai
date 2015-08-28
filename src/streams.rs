496b9c96ce9931f772ef47d6484dd7079e0881db
Zack Slayton <zslayton@us.ibm.com>
2015-08-28 18:11:55 -0400

    Readiness testing

--- src/streams.rs
+++ src/streams.rs
@@ -11,34 +11,48 @@ use Buffer;
 //TODO: Provide impl for T where T: Evented + Read + Write
 pub trait EventedByteStream : Evented + Read + Write {
   fn on_ready(&mut self);
-  fn on_readable_bytes(&mut self, &mut Buffer);
-  fn on_writable_bytes(&mut self, &Buffer);
+  fn on_readable(&mut self, &mut Buffer);
+  fn on_writable(&mut self, &Buffer);
   fn on_error(&mut self);
 }
 
 impl <T> EventedByteStream for T where T: Evented + Read + Write {
   fn on_ready(&mut self) {
-    debug!("Impl ready");
+    debug!("EventedByteStream ready");
   }
 
-  fn on_readable_bytes(&mut self, buffer: &mut Buffer) {
-    debug!("Impl ready");
+  fn on_readable(&mut self, buffer: &mut Buffer) {
+    debug!("EventedByteStream readable");
   }
 
-  fn on_writable_bytes(&mut self, buffer: &Buffer) {
-    debug!("Impl ready");
+  fn on_writable(&mut self, buffer: &Buffer) {
+    debug!("EventedByteStream writable");
   }
 
   fn on_error(&mut self) {
-    debug!("Impl ready");
+    debug!("EventedByteStream error");
   }
 }
 
 #[derive(Debug)]
+enum StreamState {
+  NotReady,
+  Ready,
+  Error,
+  Closed
+}
+
+#[derive(Debug)]
 pub struct EventedFrameStream<E> where E: EventedByteStream {
-  stream: E, 
+  stream: E,
+  state: StreamState,
   // TODO: Could be Option<Recycled<'a, Buffer>> with lifetimes
   buffer: Option<RcRecycled<Buffer>>,
+}
+
+pub trait FrameHandler {
+  fn on_frame_received(&mut self);
+  fn on_frame_written(&mut self);
 }
 
 pub struct FrameEngineBuilder<E> where E: EventedByteStream {
@@ -65,6 +79,7 @@ impl <E> FrameEngineBuilder<E> where E: EventedByteStream {
  pub fn manage(&mut self, evented_byte_stream: E) {
     let evented_frame_stream = EventedFrameStream {
       stream: evented_byte_stream,
+      state: StreamState::NotReady,
       buffer: None
     };
     // Store new stream, get token to use
@@ -72,9 +87,9 @@ impl <E> FrameEngineBuilder<E> where E: EventedByteStream {
       Ok(token) => token,
       _ => return
     };
-    let efs_ref: &EventedFrameStream<E> = self.frame_engine.streams.get(token).unwrap();
+    let efs: &EventedFrameStream<E> = self.frame_engine.streams.get(token).unwrap();
     //TODO: Handle error?
-    let _ = self.event_loop.register(&efs_ref.stream, token).unwrap();
+    let _ = self.event_loop.register(&efs.stream, token).unwrap();
   }
 
   pub fn run(self) {
@@ -84,12 +99,28 @@ impl <E> FrameEngineBuilder<E> where E: EventedByteStream {
   }
 }
 
-impl <E> Handler for FrameEngine<E> where E: Evented {
+impl <E> Handler for FrameEngine<E> where E: EventedByteStream {
   type Timeout = ();
   type Message = ();
 
   fn ready(&mut self, event_loop: &mut EventLoop<Self>, token: Token, event_set: EventSet) {
+    use self::StreamState::*;
     debug!("Token={:?} ready, EventSet={:?}", token, event_set);
+    let efs: &mut EventedFrameStream<E> = self.streams.get_mut(token).unwrap();
+
+    if event_set.is_writable() {
+      match efs.state {
+        NotReady => {
+          efs.state = Ready;
+          efs.stream.on_ready();
+        },
+        _ => {
+          // TODO: See if it already has a buffer
+          efs.stream.on_writable(&self.buffer_pool.new());
+          event_loop.shutdown();
+        }
+      }
+    }
   }
 
   fn notify(&mut self, event_loop: &mut EventLoop<Self>, message: Self::Message) {
